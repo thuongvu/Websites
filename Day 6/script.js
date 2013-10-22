3ad8e20c2c1b351b7fd785838e8f1a839755dff5
thuongvu <EagleClaw@Thunder.local>
2013-10-21 19:26:29 -0700

    Day 6 - Monochrome Delight

---
+++ Day 6/script.js
@@ -0,0 +1,32 @@
+/* when the document loads, the for loop calls addDiv x number of times */
+$(document).ready(function(){
+    for (var i = 0; i < 576; i++) {
+     addDiv();
+    };
+});
+             
+/* add a div function */
+function addDiv () {
+   // create a new div element
+    var newDiv = document.createElement("div");
+    // finds dynamicDiv and assigns it to variable mainDiv
+    mainDiv = document.getElementById("dynamicDivs");
+    // inserts the JS-created div before dynamicDiv in html
+    document.body.insertBefore(newDiv, mainDiv);
+};
+
+/* on mouseover over divs, this function toggles the "green" class */
+  $(function() {
+    $( "div" ).mouseover(function(){
+      $( this ).toggleClass("opacity", "easeInElastic");
+    });
+  });
+
+// on hover over divs, this function 
+  $(function() {
+    $( "div" ).hover(function(){
+      $( this ).toggleClass("highlight", "easeInElastic");
+    });
+  });
+
+
