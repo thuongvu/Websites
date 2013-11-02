42bc8e3d034ddc83cc5cee7c8bb59c1e15c11e9c
thuongvu <EagleClaw@thunder.gateway.pace.com>
2013-11-01 23:28:58 -0700

    Day 17 - 12 Most popular videos on youtube TODAY!

---
+++ Day 17/script.js
@@ -0,0 +1,15 @@
+function showVideos(data) {
+		var feed = data.feed;
+		var entries = feed.entry || []; // array of video entries
+		var addHTML = ['<div id="videos">'];
+
+		for (i = 0; i < entries.length; i++) {
+			var entry = entries[i]; // this cycles through the video entries on the page // got this wrong, relook at it
+			var thumbnail = entries[i].media$group.media$thumbnail[2].url;
+			var videoURL = entries[i].media$group.media$content[0].url;
+			var mediaPlayer = entries[i].media$group.media$player.url; 
+			addHTML.push('<a href="', mediaPlayer, '"><img src="', thumbnail, '" width="280" height="160" /></a>')
+						  
+		};
+		document.getElementById('videos').innerHTML = addHTML.join('');
+	}
