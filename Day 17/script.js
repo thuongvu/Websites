fda635f6bfa9544ce682cbedd58a8ab1053c53c9
thuongvu <EagleClaw@thunder.gateway.pace.com>
2013-11-03 01:01:09 -0700

    Day 18 - See You At The Movies

--- Day 17/script.js
+++ Day 17/script.js
@@ -6,7 +6,6 @@ function showVideos(data) {
 		for (i = 0; i < entries.length; i++) {
 			var entry = entries[i]; // this cycles through the video entries on the page // got this wrong, relook at it
 			var thumbnail = entries[i].media$group.media$thumbnail[2].url;
-			var videoURL = entries[i].media$group.media$content[0].url;
 			var mediaPlayer = entries[i].media$group.media$player.url; 
 			addHTML.push('<a href="', mediaPlayer, '"><img src="', thumbnail, '" width="280" height="160" /></a>')
 						  
---
+++ Day 18/script.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function() {
+		$('#moviesOpening').click(function() {
+			$('#moviesOpening').html("<span class='glyphicon glyphicon-random'>&nbsp;<span>Loading</span>");
+			$.ajax({
+				url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?apikey=cfgemr7rvkj9uvtpge95kc6d&limit=16",
+				dataType: "jsonp",
+				jsonpCallback: "opening", 
+				complete: function(){
+					$('#moviesOpening').html("<span class='glyphicon glyphicon-film'></span>&nbsp;&nbsp;<span>Just Opening</span>")	
+				}
+			});
+		});
+
+		$('#moviesInTheaters').click(function() {
+			$('#moviesInTheaters').html("<span class='glyphicon glyphicon-random'>&nbsp;<span>Loading</span>");
+			$.ajax({
+				url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json?apikey=cfgemr7rvkj9uvtpge95kc6d&page_limit=16",
+				dataType: "jsonp",
+				jsonpCallback: "inTheaters", 
+				complete: function(){
+					$('#moviesInTheaters').html("<span class='glyphicon glyphicon-film'></span>&nbsp;&nbsp;<span>In Theaters Now</span>")
+				}
+			});
+		});
+
+		$('#moviesUpcoming').click(function() {
+			$('#moviesUpcoming').html("<span class='glyphicon glyphicon-random'>&nbsp;<span>Loading</span>");
+			$.ajax({
+				url: "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/upcoming.json?apikey=cfgemr7rvkj9uvtpge95kc6d&page_limit=16",
+				dataType: "jsonp",
+				jsonpCallback: "upcoming", 
+				complete: function(){
+					$('#moviesUpcoming').html("<span class='glyphicon glyphicon-film'></span>&nbsp;&nbsp;<span>Coming Soon</span>")
+				}
+			});
+		});		
+	});
+
+
+
+function opening(response) {
+	$('#list').empty();
+	$.each(response.movies, function(i, movie) {
+		$('#list').append("<a href ='" +movie.links.alternate+ "'><img class='poster' src='" +movie.posters.detailed+ "' height='203' width='140' </></a>");
+	});
+}
+function inTheaters(response) {
+	$('#list').empty();
+	$.each(response.movies, function(i, movie) {
+		$('#list').append("<a href ='" +movie.links.alternate+ "'><img class='poster' src='" +movie.posters.detailed+ "' height='203' width='140' </></a>");
+	});
+}
+function upcoming(response) {
+	$('#list').empty();
+	$.each(response.movies, function(i, movie) {
+		$('#list').append("<a href ='" +movie.links.alternate+ "'><img class='poster' src='" +movie.posters.detailed+ "' height='203' width='140' </></a>");
+	});
+}
+
