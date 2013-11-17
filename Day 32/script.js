8641f78e4369c94cb33590a8bdff0f208c39bb7a
thuongvu <EagleClaw@thunder.gateway.pace.com>
2013-11-17 14:08:08 -0800

    Day 32 - update
    I successfully used ng-repeat for the first time!

--- Day 32/script.js
+++ Day 32/script.js
@@ -1,24 +1,19 @@
 function multiplication ($scope) {
 	$scope.table = {startNum: 1};
 
-	$scope.times = function() {
-		$scope.table.result1 = $scope.table.startNum * 1;
-		$scope.table.result2 = $scope.table.startNum * 2;
-		$scope.table.result3 = $scope.table.startNum * 3;
-		$scope.table.result4 = $scope.table.startNum * 4;
-		$scope.table.result5 = $scope.table.startNum * 5;
-		$scope.table.result6 = $scope.table.startNum * 6;
-		$scope.table.result7 = $scope.table.startNum * 7;
-		$scope.table.result8 = $scope.table.startNum * 8;
-		$scope.table.result9 = $scope.table.startNum * 9;
-		$scope.table.result10 = $scope.table.startNum * 10;
-		$scope.table.result11 = $scope.table.startNum * 11;
-		$scope.table.result12 = $scope.table.startNum * 12; 
-
-		// why does this not work?
-		// for (i = 1; i<12; i++) {
-		// 	$scope.table.result[i] = $scope.table.startNum * i;
-		// }
-	}
+	$scope.numberArray = [
+		{ number: 1 },
+		{ number: 2 },
+		{ number: 3 },
+		{ number: 4 },
+		{ number: 5 },
+		{ number: 6 },
+		{ number: 7 },
+		{ number: 8 },
+		{ number: 9 },
+		{ number: 10 },
+		{ number: 11 },
+		{ number: 12 },
+	]; 
 
 }
