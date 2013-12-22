6208c1c39246c8e9e9d11da97bb7b481ea8253fc
thuongvu <thuongvuho@gmail.com>
2013-12-22 15:55:23 -0800

    added a jade view

--- Node/server.js
+++ Node/server.js
@@ -10,12 +10,22 @@ app.set("ipaddr", "127.0.0.1");
 // sever port #
 app.set("port", 8080);
 
+// specifify views folder
+app.set("views", __dirname + "/views");
+
+// view engine is jade
+app.set("view engine", "jade");
+
+// specify where static content is
+app.use(express.static("public", __dirname + "/public"));
+
 app.use(express.bodyParser());
 
 // server routing
 app.get("/", function (request, response) {
 
-	response.send("server is up and running");
+	//render the view called "index"
+	response.render("index");
 
 });
 
