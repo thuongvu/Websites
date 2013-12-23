1b30d850ae92b52442c5a5a663fa1c3f99e65a1b
thuongvu <thuongvuho@gmail.com>
2013-12-22 18:26:48 -0800

    added events client <-> server

--- Node/server.js
+++ Node/server.js
@@ -5,6 +5,9 @@ var express = require("express")
 	, io = require("socket.io").listen(http)
 	, _ = require("underscore");
 
+// list of participants in chatroom
+var participants = []
+
 // server config
 
 app.set("ipaddr", "127.0.0.1");
@@ -42,8 +45,40 @@ app.post("/message", function (request, response) {
 		return response.json(400, {error: "Message is invalid"});
 	}
 
+	// we also expect the sender's name with the message
+	var name = request.body.name;
+
+	// let chatroom know there is a new message
+	io.sockets.emit("incomingMessage", {message: message, name: name});
+
 	// let the client know
 	response.json(200, {message: "Message received"});
+
+});
+
+// socket.io events
+io.on("connection", function(socket) {
+	// when a new user connects, we expect an event "newUser"
+	// then we emit an event "newConnections" w/ list
+	socket.on("newUser", function (data) {
+		participants.push({id: data.id, name: data.name});
+		io.sockets.emit("newConnection", {participants: participants});
+	});
+
+	// when a user changes name, expect event "nameChange"
+	// emit an event called "nameChanged" to all w/ id+new name of user who emited original msg
+	socket.on("nameChange", function (data) {
+		_.findWhere(participants, {id: socket.id}).name = data.name;
+		io.sockets.emit("nameChanged", {id: data.id, name: data.name});
+	});
+
+	// when user disconnects, event "disconnect" is automatically captured by server
+	// then emit event called userDisconnected to all w/ id of user who disconnected
+	socket.on("disconnect", function () {
+		participants = _.without(participants, _.findWhere(participants, {id: socket.id}));
+		io.sockets.emit("userDisconnected", {id: socket.id, sender: "system"});
+	});
+
 
 })
 
