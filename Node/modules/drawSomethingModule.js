0877a16f2bcea9f6c0967d67f05237ec07222010
thuongvu <thuongvuho@gmail.com>
2013-12-29 22:46:25 -0800

    styled + cleaned up

--- Node/modules/drawSomethingModule.js
+++ Node/modules/drawSomethingModule.js
@@ -4,18 +4,14 @@ var db = mongojs('test', ['drawSomethingCol']);
 
 function drawSomething_io (socket, io) {
 	socket.on("newStroke", function (data) {
-		// console.log(data.strokes)
-		// console.log(data.strokes)
 		currentStrokes = data.strokes
 		currentStrokes.forEach(function (obj) {
-			// console.log(obj)
 			db.drawSomethingCol.save({dot: obj}, function(err, saved) {
 			  if( err || !saved ) console.log("dot not saved in db");
 			  else console.log("dot saved in db");
 			});
 		})
 		this.broadcast.emit("strokesToDraw", {data: data});
-		// this.emit("strokesToDraw", {data: data});
 	});
 
 	socket.on("reset", function (){
@@ -24,16 +20,8 @@ function drawSomething_io (socket, io) {
 	})
 }
 
-
-
 function renderPage (request, response) {
-	// db.drawSomethingCol.find(function(err, docs) {
-   // response.render("drawSomething/drawSomething", {dots: docs.dot });
-   // response.render("drawSomething/drawSomething", {dots: "hello" });
    response.render("drawSomething/drawSomething");
-   // response.send({dots: 'json'})
-   // response.render("drawSomething/drawSomething");
-	// });
 }
 
 function getPrevious (request, response) {
