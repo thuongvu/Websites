c1ec76582be9f14f4ba9f300db068b615e8f6a15
thuongvu <thuongvuho@gmail.com>
2014-01-01 15:10:48 -0800

    sockets up, emitting, one master over 1+ slaves work.  no reciprication yet

--- Node/modules/youSeeModule.js
+++ Node/modules/youSeeModule.js
@@ -7,20 +7,27 @@ function youSee_io (socket, io) {
 	socket.on("stateChange", function (data) {
 		console.log(data)
 		this.broadcast.emit("stateBroadcast", data);
-		this.emit("stateBroadcast", data);
+		// this.emit("stateBroadcast", data);
 	})
 
 	// loadVideo
 	socket.on("loadVideo", function (data) {
 		console.log(data)
 		this.broadcast.emit("loadVideoBroadcast", data);
-		this.emit("loadVideoBroadcast", data);
+		// this.emit("loadVideoBroadcast", data);
 	})
 
 	socket.on('connect', function (data) {
 		this.broadcast.emit("askState");
-		this.emit("askState");
+		// this.emit("askState");
 	})
+
+	socket.on('currentTime', function (data) {
+		this.broadcast.emit("currentTimeBroadcast", data);
+		// this.emit("currentTimeBroadcast", data);
+	})
+
+
 }
 // socket.on("newUser", function (data) {
 // 	participants.push({id: data.id, name: data.name});
--- Node/public/js/youSee/youSeeClient.js
+++ Node/public/js/youSee/youSeeClient.js
@@ -21,10 +21,9 @@ $(document).ready(function() {
 
 	socket.on('loadVideoBroadcast', function (data) {
 		console.log(data)
-	});
 
-	socket.on('stateBroadcast', function (data) {
-		console.log(data.url)
+		ytplayer = document.getElementById("youtubePlayer");
+		console.log(data)
 		var youtubeURL = data.url
 		var params = {
 			allowScriptAccess: "always",
@@ -33,11 +32,57 @@ $(document).ready(function() {
 		var attObj = {id: "youtubePlayer"}; 
 		swfobject.embedSWF("http://www.youtube.com/v/" +youtubeURL + "?enablejsapi=1&playerapiid=ytplayer&version=3", 
 					"ytapiplayer", "480", "360", "8", null, null, params, attObj);
+
+
+
 	});
 
-	socket.on('askState', function (data) {
-		socket.emit('stateChange', {state: newState, time: currentTime, url: youtubeURL});
-	})
+	socket.on('stateBroadcast', function (data) {
+		function setVideo(callback) {
+			ytplayer = document.getElementById("youtubePlayer");
+		console.log(data)
+		var youtubeURL = data.url
+		var params = {
+			allowScriptAccess: "always",
+			allowFullScreen: true
+		};
+		var attObj = {id: "youtubePlayer"}; 
+		swfobject.embedSWF("http://www.youtube.com/v/" +youtubeURL + "?enablejsapi=1&playerapiid=ytplayer&version=3", 
+					"ytapiplayer", "480", "360", "8", null, null, params, attObj);
+
+		setTimeout(function () {
+			// ytplayer.seekTo(seekTime)
+			callback()
+		}, 1000)
+		
+		}
+
+		setVideo(syncState)
+		
+		function syncState() {
+			socket.on('currentTimeBroadcast', function(data) {
+				console.log("the current time is " + data.currentTime)
+				seekTime = data.currentTime 
+			})
+
+			if (data.state === 1) {
+				console.log("started")
+				ytplayer.seekTo(seekTime)
+				ytplayer.playVideo();
+			} else {
+				if (data.state === 2) {
+					ytplayer.pauseVideo()
+					ytplayer.seekTo(seekTime)
+				}
+			}
+		}
+		
+
+	});
+
+	// socket.on('askState', function (data) {
+	// 	socket.emit('stateChange', {state: newState, time: currentTime, url: youtubeURL});
+	// })
 
 });
 
@@ -49,6 +94,13 @@ function onYouTubePlayerReady(playerId) {
 		console.log("Hello")
 		var currentTime;
 		var currentState;
+
+		
+		// setInterval(function() {
+		// 	currentTime = ytplayer.getCurrentTime();
+		// 	socket.emit("currentTime", {currentTime: currentTime})
+		// }, 1000)
+
 
 		// setInterval(function() {
 		// currentTime = ytplayer.getCurrentTime();
@@ -64,5 +116,14 @@ function onYouTubePlayerReady(playerId) {
 	   	var currentTime = ytplayer.getCurrentTime();
 	   	// console.log("Player's new state: " + newState + " time is " + currentTime);
 	   	// console.log(youtubeURL)
+	   	if (newState === 1) {
+	   		 time = setInterval(function() {
+	   			currentTime = ytplayer.getCurrentTime();
+	   			socket.emit("currentTime", {currentTime: currentTime})
+	   		}, 1000)
+	   	} else {
+	   		clearInterval(time)
+	   	}
+
 	   	socket.emit('stateChange', {state: newState, time: currentTime, url: youtubeURL});
 		}
