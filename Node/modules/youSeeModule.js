3bfe7594a72f4f5ddfd2546ed3f603a5ebcbf760
thuongvu <thuongvuho@gmail.com>
2014-01-01 22:39:52 -0800

    deleted commented areas, check all global vars

--- Node/modules/youSeeModule.js
+++ Node/modules/youSeeModule.js
@@ -1,49 +1,31 @@
+var currentTimeData;
+var urlData;
 
 function renderPage (request, response) {
 	response.render("youSee/youSee")
 }
-var currentTimeData;
-var urlData;
+
 function youSee_io (socket, io) {
 	socket.on("stateChange", function (data) {
-		// console.log(data)
 		this.broadcast.emit("stateBroadcast", data);
-		// this.emit("stateBroadcast", data);
 	})
 
-	// loadVideo
 	socket.on("loadVideo", function (data) {
-		// console.log(data)
 		this.broadcast.emit("loadVideoBroadcast", data);
-		// this.emit("loadVideoBroadcast", data);
 		urlData = data;
 	})
 
 	socket.on('currentTime', function (data) {
 		this.broadcast.emit("currentTimeBroadcast", data);
-		// this.emit("currentTimeBroadcast", data);
 		currentTimeData = data;
 	})
 
 	socket.on('connected', function () {
 		this.emit("askState", {currentTime: currentTimeData, url: urlData})
-		// currentTimeData = "";
-		// urlData = "";
-
-		// }
-		// this.emit("askState");
+		currentTimeData = "";
+		urlData = "";
 	})
-
-
-
-
 }
-// socket.on("newUser", function (data) {
-// 	participants.push({id: data.id, name: data.name});
-// 	this.broadcast.emit("newConnection", {participants: participants});
-// 	this.emit("newConnection", {participants: participants});
-// });
-
 
 exports.renderPage = renderPage;
 exports.youSee_io = youSee_io;
--- Node/public/js/youSee/youSeeClient.js
+++ Node/public/js/youSee/youSeeClient.js
@@ -22,23 +22,13 @@ $(document).ready(function() {
 	$('#loadVideo').click(function(){
 		youtubeURL = $('#urlInput').val(); 
 		loadVideo(youtubeURL)
-		// var params = {
-		// 	allowScriptAccess: "always",
-		// 	allowFullScreen: true
-		// };
-		// var attObj = {id: "youtubePlayer"}; 
-		// swfobject.embedSWF("http://www.youtube.com/v/" +youtubeURL + "?enablejsapi=1&playerapiid=ytplayer&version=3", 
-		// 			"ytapiplayer", "480", "360", "8", null, null, params, attObj);
 		socket.emit("loadVideo", {url: youtubeURL})
 	});
 
-	// socket.emit("connected")
 
 	socket.on('askState', function (data) {
 		console.log(data)
 		loadVideo(data.url.url)
-		// console.log(ytplayer)
-		// var ytplayer = document.getElementById("youtubePlayer");
 		setTimeout(function ( ){
 			ytplayer.seekTo(data.currentTime.currentTime + 3)
 			ytplayer.playVideo();
@@ -123,10 +113,6 @@ $(document).ready(function() {
 
 	});
 
-	// socket.on('askState', function (data) {
-	// 	socket.emit('stateChange', {state: newState, time: currentTime, url: youtubeURL});
-	// })
-
 });
 
 
@@ -138,33 +124,16 @@ function onYouTubePlayerReady(playerId) {
 		var currentTime;
 		var currentState;
 
-		
-		// setInterval(function() {
-		// 	currentTime = ytplayer.getCurrentTime();
-		// 	socket.emit("currentTime", {currentTime: currentTime})
-		// }, 1000)
-
-
-		// setInterval(function() {
-		// currentTime = ytplayer.getCurrentTime();
-		// currentState = ytplayer.getPlayerState()
-		// console.log(currentState + " " + currentTime )
-		// }, 1000)
-
 		ytplayer.addEventListener("onStateChange", "onytplayerStateChange");
 
 		
 	}
 		function onytplayerStateChange(newState) {
-	   	// var currentTime = ytplayer.getCurrentTime();
-	   	// console.log("Player's new state: " + newState + " time is " + currentTime);
-	   	// console.log(youtubeURL)
 	   	if (newState === 1) {
 	   		console.log("state is " + newState)
 	   		 time = setInterval(function() {
 	   			var currentTime = ytplayer.getCurrentTime();
 	   			socket.emit("currentTime", {currentTime: currentTime})
-	   			// socket.emit('stateChange', {state: newState, time: currentTime});
 	   		}, 1000)
 	   	} else if (newState === 2) {
 	   		console.log("state is " + newState)
