eaaeb4ac0ecb68840f6c3663c15248e06f6d9570
thuongvu <thuongvuho@gmail.com>
2014-01-06 22:20:15 -0800

    test igmap on the server

--- Node/modules/igMapModule.js
+++ Node/modules/igMapModule.js
@@ -3,52 +3,39 @@ var events = require("events");
 var _ = require("underscore");
 var eventEmitter = new events.EventEmitter();
 var url;
-var photoCounter = 0;
 var subscriptions = [];
+var subDirectory = {};
+var subscriptionID;
 
 // you can already see my keys in my git history, so fuck it.
 Instagram.set('client_id', 'ee792378f4f2484b81748343d1bba08b');
 Instagram.set('client_secret', '4c36bd0b64214355abfeb6b80cf8960b');
-Instagram.set('callback_url', 'http://181e0fc6.ngrok.com/igmap/callback/1');
+// Instagram.set('callback_url', 'http://181e0fc6.ngrok.com/igmap/callback/1');
+Instagram.set('callback_url', 'http://node.thuongvuho.com/igmap/callback/1');
 
-// https://api.instagram.com/v1/subscriptions?client_secret=4c36bd0b64214355abfeb6b80cf8960b&client_id=ee792378f4f2484b81748343d1bba08b
-// curl -X DELETE 'https://api.instagram.com/v1/subscriptions?client_secret=e75bf8d778ea4b25b5037d5d65ba3f4b&object=all&client_id=7f3a0d9e6cca4689b5dadeaed96197dd'
-
-// function igGeo() {
-// 	Instagram.subscriptions.subscribe({
-// 	  object: 'geography',
-// 	  aspect: 'media',
-// 	  lat: '35.657872',
-// 	  lng: '139.70232',
-// 	  radius: '2000',
-// 	  type: 'subscription',
-// 	  id: '#'
-// 	});
-// }
-
-
-function igGeo(lat, lng) {
-	// Instagram.subscriptions.list();
-	for (var i = 0; i < subscriptions.length; i++) {
-		Instagram.subscriptions.unsubscribe({ id: subscriptions[i] })
+function igGeo(lat, lng, socketId) {
+	if (subDirectory[socketId]) {
+		var previousSub = subDirectory[socketId];
+		Instagram.subscriptions.unsubscribe({ id: previousSub })
 	}
-	// Instagram.subscriptions.unsubscribe({ id: 4585406 })
-	// Instagram.subscriptions.unsubscribe_all();
 
-	// showList
-
-	// Instagram.subscriptions.unsubscribe({ id: 4030559 });
-	// Instagram.subscriptions.unsubscribe_all();
-	// Instagram.media.unsubscribe_all();
+	console.log("socketId is + " + socketId)
 
 	Instagram.subscriptions.subscribe({
 	  object: 'geography',
 	  aspect: 'media',
 	  lat: lat,
 	  lng: lng,
-	  radius: '2000',
+	  radius: '4000',
 	  type: 'subscription',
-	  id: '#'
+	  id: '#',
+	  complete: function (data) {
+	  	console.log("subscription is " +  data.id)
+	  	subDirectory[socketId] = data.id
+	  	console.log(subDirectory)
+	  	eventEmitter.emit("newSubscription", {data: data.id, id: socketId});
+	  	
+	  }
 	});
 }
 
@@ -63,44 +50,46 @@ function handshake(request, response) {
 
 function igPost(request, response) {
 	var data = request.body;
-	// subscriptions.push((data.subscription_id);
-	// console.log(subscriptions)
-	console.log(data)
+	console.log(data);
+
 	data.forEach(function(tag) {
 		url = 'https://api.instagram.com/v1/geographies/' + tag.object_id + '/media/recent?&count=1&client_id=ee792378f4f2484b81748343d1bba08b';
-		// if (_.contains(subscriptions, tag.subscription_id)) {
-		// 	console.log("already has " + tag.subscription_id)
-		// } else {
-		// 	subscriptions.push(tag.subscription_id);
-		// }
+		subscriptionID = tag.subscription_id;
 		if (!(_.contains(subscriptions, tag.subscription_id))) {
 			subscriptions.push(tag.subscription_id);
 		}
-		eventEmitter.emit("newPhoto");
+		eventEmitter.emit("sendPhoto");
 	})
 
 	response.end()
 }
 
-eventEmitter.on("newPhoto", function () {
-	// photoCounter++;
-	// if (photoCounter === 10) {
-		eventEmitter.emit("sendPhoto")
-		// photoCounter = 0;
-	// }
-})
-
-
 function igMap_io (socket,io) {
 	eventEmitter.on("sendPhoto", function () {
-		socket.emit("newPhoto", {data: url});
+		socket.emit("newPhoto", {data: url, subscriptionID: subscriptionID});
 	})
 
 	socket.on("newLocation", function (data){
-		console.log(data.lat)
-		igGeo(data.lat, data.lng)
+		igGeo(data.lat, data.lng, data.id)
 	})
 
+	socket.emit("connection", {id: socket.id})
+
+	eventEmitter.on("newSubscription", function (data) {
+		var subscription = data.data;
+		var socketId = data.id;
+		socket.emit("newSubscription", {data: subscription, socketId: socketId});
+	})
+	
+	socket.on("disconnect", function () {
+		var socketId = socket.id;
+		if (subDirectory[socketId]) {
+		var previousSub = subDirectory[socketId];
+		Instagram.subscriptions.unsubscribe({ id: previousSub })
+		console.log("this is what is left of subdirectory")
+		console.log(subDirectory)
+	}
+	})
 
 
 }
@@ -110,5 +99,3 @@ exports.handshake = handshake;
 exports.igPost = igPost;
 exports.igGeo = igGeo;
 exports.igMap_io = igMap_io;
-
-
--- Node/public/js/igMap/igMapClient.js
+++ Node/public/js/igMap/igMapClient.js
@@ -2,96 +2,110 @@ $(document).ready(function () {
 	var serverBaseUrl = document.domain;
 	var socket = io.connect(serverBaseUrl + '/igmap');
 	var image;
+	var socketId;
+	var locations = [];
+	var circle;
+	var subsID;
+	var map;
+
+	socket.on("connection", function(data) {
+		socketId = data.id;
+		console.log(socketId)
+	})
+
+	socket.on("newSubscription", function (data) {
+		if (data.socketId == socketId) {
+			subsID = data.data;
+			console.log(subsID + " is my latest subscription ID")
+		}
+	})
 
 	var happyMarker = L.AwesomeMarkers.icon({ icon: 'star', markerColor: 'orange' })
 
-	function streamPhotos() {
-		socket.on('newPhoto', function (data) {
-			var url = data.data;
-			console.log(data)
-			req =	$.ajax({
-					url: url,
-					type: 'GET',
-					crossDomain: true,	
-					dataType: 'jsonp'
-				}).done(function(data) {
-					for (var i = 0; i < data.data.length; i++) {
-						console.log(data)
-						console.log(data.data[0].location.latitude)
-						console.log(data.data[0].location.longitude)
-						image = data.data[i].images.low_resolution.url;
-						// $('#imageContainer').prepend('<img src="' + image + '" />')
+			socket.on('newPhoto', function (data) {
+				console.log("arrival for data.subscriptionID:" + data.subscriptionID)
+				console.log("my subscriptionId is " + subsID)
+				if (data.subscriptionID == subsID) {
+					var url = data.data;
+					console.log(data)
+					req =	$.ajax({
+							url: url,
+							type: 'GET',
+							crossDomain: true,	
+							dataType: 'jsonp'
+						}).done(function(data) {
+							for (var i = 0; i < data.data.length; i++) {
+								console.log(data)
+								console.log(data.data[0].location.latitude)
+								console.log(data.data[0].location.longitude)
+								image = data.data[i].images.low_resolution.url;
+								var marker = L.marker([data.data[0].location.latitude, data.data[0].location.longitude], {icon: happyMarker, opacity: 0.7}).addTo(map);
+								marker.bindPopup("<img src='"+ data.data[0].images.thumbnail.url +"'></img>").openPopup();
 
-						var marker = L.marker([data.data[0].location.latitude, data.data[0].location.longitude], {icon: happyMarker, opacity: 0.7}).addTo(map);
-						marker.bindPopup("<img src='"+ data.data[0].images.thumbnail.url +"'></img>").openPopup();
-
-					}
-				})
-		});
-	}
+							}
+						})
+				}
+				
+			});
 	
+	if (navigator.geolocation) {
+	     navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+	} else {
+	    $(".map").text("Your browser isn't supported, sorry :(")
+	}
 
+	//callback of geolocate for error
+	function positionError ( ){
+	  console.log("Sorry, it doesn't work")
+	}
 
-
-
-
-	streamPhotos();
-
-	$('#pause').click(function() {
-		socket.removeAllListeners("data")
-		console.log("paused stream")
-	})
-	$('#resume').click(function() {
-		streamPhotos();
-	})
-	$('#addStream').click(function() {
-		var lat = $('#latitude').val();
-		var lng = $('#longitude').val();
-		// addStream(37.7833, -122.4167);
-		addStream(lat, lng);
-	})
+	//callback of geolocate for success
+	function positionSuccess(position) {
+	  var lat = position.coords.latitude;
+	  var lng = position.coords.longitude;
+	  makeMap(lat,lng)                        
+	}
 
 	function addStream(lat, lng) {
-		socket.emit("newLocation", {lat: lat, lng: lng});
+		socket.emit("newLocation", {lat: lat, lng: lng, id: socketId});
 	}
 
+	function makeMap(lat, lng) {
+		$('#map').css("width", $(window).width() - 100)
+					.css("height", $(window).height() - 100 )
 
-var locations = [];
-var circle;
+			map = L.map('map').setView([lat, lng], 12);
 
-	$('#map').css("width", $(window).width() - 100)
-				.css("height", $(window).height() - 100 )
+		   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+		        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+		   }).addTo(map);
 
-		// var map = L.map('map').setView([0, 0], 2);
-		var map = L.map('map').setView([40.69, -73.99], 12);
-		// 40.6928° N, 73.9903° W
+		   circle = L.circle([lat, lng], 4000, {
+		   	color: 'red',
+		   	fillColor: 'red',
+		   	fillOpacity: 0.3,
+		   }).addTo(map);
 
-	   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	   }).addTo(map);
+		   addStream(lat, lng);
+		   map.on('click', function (e) {
+		   	console.log("you clicked at lat: " + e.latlng)
+		   	locations = [];
+		   	locations.push(e.latlng);
+		   	var latClick = locations[0].lat;
+		   	var lngClick = locations[0].lng;
 
-	 function onMapClick(e) {
-	 	console.log("you clicked at lat: " + e.latlng)
-	 	locations = [];
-	 	locations.push(e.latlng);
-	 	var latClick = locations[0].lat;
-	 	var lngClick = locations[0].lng;
+		   	if (circle) {
+		   		map.removeLayer(circle);
+		   	}
+		   	
+		   	circle = L.circle([latClick, lngClick], 4000, {
+		   		color: 'red',
+		   		fillColor: 'red',
+		   		fillOpacity: 0.3,
+		   	}).addTo(map);
 
-	 	if (circle) {
-	 		map.removeLayer(circle);
-	 	}
-	 	
-	 	circle = L.circle([latClick, lngClick], 2000, {
-	 		color: 'red',
-	 		fillColor: 'red',
-	 		fillOpacity: 0.3,
-	 	}).addTo(map);
-
-	 	addStream(latClick, lngClick);
-
-	 }
-
-	 map.on('click', onMapClick);
-
+		   	addStream(latClick, lngClick);
+		   });
+	}
 
 })
