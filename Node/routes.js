a94c1ec39f74204faa5ba067c5e5e77767293232
thuongvu <thuongvuho@gmail.com>
2014-01-17 00:33:19 -0800

    will this really work?

--- Node/routes.js
+++ Node/routes.js
@@ -210,27 +210,34 @@ module.exports = function (app, passport) {
 				  '/statuses/home_timeline',
 				  {count: 30, trim_user: true},
 				function logResponse(error, data, response) {
-					// for (var i = 0; i < data.length; i++) {
-					// 	if (data[i].text.length > 1) {
-					// 			var split = data[i].text.split(" ");
-					// 			for (var j = 0; j < split.length; j++) {
-					// 				hashtags.push(split[j])
-					// 			}
-					// 	}
-					// }
-				if(req.user) {
-				    role = 1;
-				    username = req.user.twitter.username;
-				    console.log(req.user)
+					function hashTags(data, callback) {
+						for (var i = 0; i < data.length; i++) {
+							if (data[i].text.length > 1) {
+									var split = data[i].text.split(" ");
+									for (var j = 0; j < split.length; j++) {
+										hashtags.push(split[j])
+									}
+							}
+						}
+						callback()
+					}
+					hashTags(function() {
+						if(req.user) {
+						    role = 1;
+						    username = req.user.twitter.username;
+						    console.log(req.user)
 
-				    res.cookie('user', JSON.stringify({
-				        'username': username,
-				        'role': role,
-				        'hashtags': ["work"],
-				    }));
+						    res.cookie('user', JSON.stringify({
+						        'username': username,
+						        'role': role,
+						        'hashtags': hashtags,
+						    }));
 
-				    res.redirect('/twittext');
-				}
+						    res.redirect('/twittext');
+						}
+					})
+					
+				
 				
 				// console.log(res)
 			   
