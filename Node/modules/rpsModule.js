a19a239da79f95711084a00302e77c2706d7f2be
thuongvu <thuongvuho@gmail.com>
2014-01-19 17:50:18 -0800

    images, messages for users to wait for more players, disconnect server logic

--- Node/modules/rpsModule.js
+++ Node/modules/rpsModule.js
@@ -16,17 +16,31 @@ function rps_io (socket, io) {
 	})
 
 	socket.on("joinRoom", function(data) {
+		var roomName = data.roomName;
 		console.log(data.id + " is joining room " + data.roomName)
 		var room = data["roomName"];
 		socket.join(room)
 
 		if (!(newGame[room])) {
+			// create a new room if it doesnt exist and assign user1 to object
 			newGame[room] = new Game(room);
 			newGame[room].user1 = data.id;
 			console.log(newGame)
+			// broadcast to user that they are waiting for a new player
+			socket.in(roomName).emit('waitForNewPlayer', "waiting for another player")
+			// raise usercount
+			newGame[room].userCount ++;
+			console.log(newGame[room].room + " 's usercount is " + newGame[room].userCount)
 		} else {
+			// assign user2 to that specific room's object
 			newGame[room].user2 = data.id;
 			console.log(newGame)
+			// broadcast this to both players
+			socket.in(roomName).broadcast.emit('waitForNewPlayer', "new player has joined!  begin!  choose!")
+			socket.in(roomName).emit('waitForNewPlayer', "two players in this room!  begin!  choose!")
+			// raise usercount
+			newGame[room].userCount ++;
+			console.log(newGame[room].room + " 's usercount is " + newGame[room].userCount)
 		}
 	})
 
@@ -41,37 +55,106 @@ function rps_io (socket, io) {
 		if (newGame[roomName].user1 === id) {
 			newGame[roomName].user1Strategy = strategy;
 			console.log(newGame[roomName]);
+			socket.in(roomName).emit('chooseWait', "waiting for the user other")
+			socket.in(roomName).broadcast.emit('chooseWait', "the other player is waiting for you")
 		} else if (newGame[roomName].user2 === id) {
 			newGame[roomName].user2Strategy = strategy;
 			console.log(newGame[roomName]);
+			socket.in(roomName).emit('chooseWait', "waiting for the user other")
+			socket.in(roomName).broadcast.emit('chooseWait', "the other player is waiting for you")
 		}
 
 		if (newGame[roomName].user1Strategy && newGame[roomName].user2Strategy) {
 			console.log("they've both chosen strategies now")
 			if ((newGame[roomName].user1Strategy === 'rock') && (newGame[roomName].user2Strategy === 'rock')) {
 				console.log("tie bc rock === rock")
+				socket.in(roomName).emit('bothChosen', "tie bc rock === rock")
+				socket.in(roomName).broadcast.emit('bothChosen', "tie bc rock === rock")
 			} else if ((newGame[roomName].user1Strategy === 'rock') && (newGame[roomName].user2Strategy === 'scissors')) {
 				console.log("player 1 wins, bc rock > scissors")
+				socket.in(roomName).emit('bothChosen', "player 1 wins, bc rock > scissors")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 1 wins, bc rock > scissors")
 			} else if ((newGame[roomName].user1Strategy === 'rock') && (newGame[roomName].user2Strategy === 'paper')) {
 				console.log("player 2 wins, bc rock < paper")
+				socket.in(roomName).emit('bothChosen', "player 2 wins, bc rock < paper")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 2 wins, bc rock < paper")
 			} else if ((newGame[roomName].user1Strategy === 'scissors') && (newGame[roomName].user2Strategy === 'rock')) {
 				console.log("player 2 wins, bc scissors < rock")
+				socket.in(roomName).emit('bothChosen', "player 2 wins, bc scissors < rock")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 2 wins, bc scissors < rock")
 			} else if ((newGame[roomName].user1Strategy === 'scissors') && (newGame[roomName].user2Strategy === 'scissors')) {
 				console.log("tie, bc scissors === scissors")
+				socket.in(roomName).emit('bothChosen', "bc scissors === scissors")
+				socket.in(roomName).broadcast.emit('bothChosen', "bc scissors === scissors")
 			} else if ((newGame[roomName].user1Strategy === 'scissors') && (newGame[roomName].user2Strategy === 'paper')) {
 				console.log("player 1 wins, bc scissors < paper")
+				socket.in(roomName).emit('bothChosen', "player 1 wins, bc scissors < paper")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 1 wins, bc scissors < paper")
 			} else if ((newGame[roomName].user1Strategy === 'paper') && (newGame[roomName].user2Strategy === 'rock')) {
 				console.log("player 1 wins, bc paper > rock")
+				socket.in(roomName).emit('bothChosen', "player 1 wins, bc paper > rock")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 1 wins, bc paper > rock")
 			} else if ((newGame[roomName].user1Strategy === 'paper') && (newGame[roomName].user2Strategy === 'scissors')) {
 				console.log("player 2 wins, bc paper < scissors")
+				socket.in(roomName).emit('bothChosen', "player 2 wins, bc paper < scissors")
+				socket.in(roomName).broadcast.emit('bothChosen', "player 2 wins, bc paper < scissors")
 			} else if ((newGame[roomName].user1Strategy === 'paper') && (newGame[roomName].user2Strategy === 'paper')) {
 				console.log("tie, bc paper === paper")
+				socket.in(roomName).emit('bothChosen', "tie, bc paper === paper")
+				socket.in(roomName).broadcast.emit('bothChosen', "tie, bc paper === paper")
 			} 
 		}  
 
-		// socket.in(data.roomName).emit('waiting', "Another player has joined!  Begin!")
-		// socket.in(data.roomName).broadcast.emit('waiting', "Another player has joined!  Begin!")
+	})
 
+	socket.on("disconnect", function() {
+		console.log(socket.id + " is leaving") 
+		for (prop in newGame) {
+			function findAndDeleteUser(callback) {
+				if (newGame[prop].user1 === socket.id) {
+					// trying to find the room name that person left
+					console.log(prop)
+					var specificRoom = prop;
+					console.log("newGame[specificRoom] before deleting user")
+					console.log(newGame[specificRoom])
+					delete newGame[specificRoom].user1
+					console.log("newGame[specificRoom] after deleting user")
+					console.log(newGame[specificRoom])
+					// take 1 from usercount
+					newGame[specificRoom].userCount --;
+					console.log("newGame[specificRoom].userCount");
+					console.log(newGame[specificRoom].userCount);
+					callback(specificRoom)
+				} else if (newGame[prop].user2 === socket.id) {
+					// trying to find the room name that person left
+					console.log(prop)
+					var specificRoom = prop;
+					console.log("newGame[specificRoom] before deleting user")
+					console.log(newGame[specificRoom])
+					delete newGame[specificRoom].user2
+					console.log("newGame[specificRoom] after deleting user")
+					console.log(newGame[specificRoom])
+					// take 1 from usercount
+					newGame[specificRoom].userCount --;
+					console.log("newGame[specificRoom].userCount");
+					console.log(newGame[specificRoom].userCount);
+					callback(specificRoom)
+				}
+				
+			}
+			findAndDeleteUser(function(specificRoom) {
+				console.log(specificRoom)
+				console.log("newGame before deleting ROOM")
+				console.log(newGame)
+				if (newGame[specificRoom].userCount === 0) {
+					delete newGame[specificRoom]
+					console.log("newGame after deleting room")
+					console.log(newGame)
+				}
+			})
+
+
+		}
 	})
 
 }
--- Node/public/js/rps/controllers.js
+++ Node/public/js/rps/controllers.js
@@ -2,7 +2,25 @@ angular.module('app.controllers', [])
 	.controller('mainCtrl', ['$scope', 'socket', function ($scope, socket) {
 		$scope.room = {};
 		$scope.user = {};
+		$scope.display = {};
+		$scope.show = {};
+		$scope.show = function(strategy) {
+			$scope.show[strategy] = true;
+
+		}
+		// $scope.show.paper = true;
 		$scope.choose = function(strategy) {
+			// logic for showing which strategy image
+			var strategies = ["paper", "scissors", "rock"];
+			for (var i = 0; i < strategies.length; i++) {
+				if (strategy === strategies[i]) {
+					$scope.show[strategy] = true;
+				}  else {
+					$scope.show[strategies[i]] = false;
+				}
+			} 
+			
+			// logic for emitting strategy to server
 			console.log(strategy)
 			socket.emit("choose", {id: $scope.user.id, roomName: $scope.room.name, strategy: strategy})
 		}
@@ -14,6 +32,21 @@ angular.module('app.controllers', [])
 			console.log(data.id)
 			$scope.user.id = data.id
 			console.log($scope.user)
+		})
+
+		socket.on("chooseWait", function(data) {
+			console.log(data)
+			$scope.display.status = data;
+		})
+
+		socket.on("bothChosen", function(data) {
+			console.log(data)
+			$scope.display.status = data;
+		})
+
+		socket.on("waitForNewPlayer", function(data) {
+			console.log(data)
+			$scope.display.status = data;
 		})
 
 	}])
