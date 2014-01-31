12fa2c29ad2190fb3c4b7560b8247d41f3eee2f3
thuongvu <thuongvuho@gmail.com>
2014-01-30 19:42:10 -0800

    sanitized beatmodule + chatroommodule

--- Node/modules/beatModule.js
+++ Node/modules/beatModule.js
@@ -1,6 +1,9 @@
+var sanitizer = require('sanitizer');
+
 function beat_io(socket, io) {
-	socket.on('beatToServer', function (data) {
-		console.log(data)
+	socket.on('beatToServer', function (dataPre) {
+		// console.log(data)
+		var data = sanitizer.sanitize(dataPre);
 		socket.broadcast.emit('beatToClient', data)
 		// socket.emit('beatToClient', data) // only for testing
 	})
--- Node/modules/chatroomModule.js
+++ Node/modules/chatroomModule.js
@@ -1,31 +1,37 @@
 var _ = require("underscore");
+var sanitizer = require('sanitizer');
 
 var participants = []
 
 function chatroomPost (request, response, io) {
-	var message = request.body.message;
+	var message = sanitizer.sanitize(request.body.message);
 	if (_.isUndefined(message) || _.isEmpty(message.trim())) {
 		return response.json(400, {error: "Message is invalid"});
 	}
-	var name = request.body.name;
+	var name = sanitizer.sanitize(request.body.name);
 	io.of('/chatroom').emit("incomingMessage", {message: message, name: name});
 	response.json(200, {message: "Message received"});
 };
 
 function chatroom_io (socket, io) {
 	socket.on("newUser", function (data) {
-		participants.push({id: data.id, name: data.name});
+		var id = sanitizer.sanitize(data.id);
+		var name = sanitizer.sanitize(data.name);
+		participants.push({id: id, name: name});
 		this.broadcast.emit("newConnection", {participants: participants});
 		this.emit("newConnection", {participants: participants});
 	});
 
 	socket.on("nameChange", function (data) {
-		_.findWhere(participants, {id: socket.id}).name = data.name;
-		this.broadcast.emit("nameChanged", {id: data.id, name: data.name});
-		this.emit("nameChanged", {id: data.id, name: data.name});
+		var id = sanitizer.sanitize(data.id);
+		var name = sanitizer.sanitize(data.name);
+		_.findWhere(participants, {id: socket.id}).name = name;
+		this.broadcast.emit("nameChanged", {id: id, name: name});
+		this.emit("nameChanged", {id: id, name: name});
 	});
 
 	socket.on("disconnect", function () {
+		var id = sanitizer.sanitize(data.id);
 		participants = _.without(participants, _.findWhere(participants, {id: socket.id}));
 		this.broadcast.emit("userDisconnected", {id: socket.id, sender: "system"});
 	});
